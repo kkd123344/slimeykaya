6256592f00b35e79171b15ea527bea01c7520cdd
kkd123344 <53440747+kkd123344@users.noreply.github.com>
2019-07-29 13:56:42 -0400

    Add files via upload

---
+++ spider.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 29 11:01:01 2019
+
+@author: STEM
+
+print("hello my name is kaya")
+print(5*2)
+print(5/2)
+print(5-2)
+print(5**2)
+print(8/9*3)
+print("5*2")
+
+"""
+def multiply(a,b):
+    multiply=a*b
+    print(multiply)
+multiply(4,5)
+
+
+def sum(a,b,c):
+    sum=a+b+c
+    print(sum)
+sum(1,2,3)
+
+def area(a,b,c):
+    area=a*b*c
+    print("the area of the triangle with base b and height c is")
+    
+area(.5,5,8)
+
